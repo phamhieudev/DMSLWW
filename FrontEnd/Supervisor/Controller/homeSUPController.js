fce619d48de527b31cb9cf534eec063d3aebe707
phamhieudev <phamhieu5969@gmail.com>
2021-02-17 10:22:45 +0700

    commit

---
+++ FrontEnd/Supervisor/Controller/homeSUPController.js
@@ -0,0 +1,35 @@
+var app = angular.module('homeSUPApp', []);
+// app.factory("checkAuth", function ($location, $rootScope) {
+//   return {
+//     getuserInfo: function () {
+//       if ($rootScope.isLoggedIlogin === undefined || $rootScope.isLoggedIlogin === null) {
+//         //$location.path('/login');
+//         location.href = "http://127.0.0.1:5501";
+//       }
+
+//     }
+//   };
+// });
+
+app.controller('homeSUPCtrl', function ($scope, $http, $rootScope) {
+
+  // $rootScope.idperson = window.localStorage.getItem("idpersonlogin");
+  // $rootScope.namelogin = window.localStorage.getItem("namelogin");
+  // $rootScope.emaillogin = window.localStorage.getItem("emaillogin");
+  // $rootScope.isLoggedIlogin = window.localStorage.getItem("isLogged");
+  // $scope.check = checkAuth.getuserInfo();
+
+  // $scope.logout = function () {
+  //   window.localStorage.clear();
+  //   $rootScope.isLoggedIlogin = false;
+  //   location.href = "http://127.0.0.1:5501";
+
+  // };
+
+  // $scope.doimatkhau = function(){
+  //   window.location.assign('http://127.0.0.1:5501/view/changepassword.html?id=' + $rootScope.idperson)
+  // }
+
+  // alert("asdasdsa");
+});
+
